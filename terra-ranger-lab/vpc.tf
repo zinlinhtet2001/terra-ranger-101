8e22170a6b8d530f3a02091d725af0df4ffa25c3
zinlinhtet2001 <zinlinhtet.du@gmail.com>
2024-08-19 14:29:05 +0630

    Edit subnet and add subnet

--- terra-ranger-lab/vpc.tf
+++ terra-ranger-lab/vpc.tf
@@ -1,17 +1,17 @@
 #Procedure of creating VPC in AWS
 #Create VPC
-#Create subnet
-#Create default route table
 #Create IGW
 #Associate IGW with VPC
+#Create subnet
+#Create default route table
 #Associate public subent with IGW
 #--------------------------------#
 
 resource "aws_vpc" "dev-vpc-01" {
     tags = {
-      Name = "dev-vpc-01"
+        Name = "dev-vpc-01"
     }
-    cidr_block = "10.0.0.0/24"    
+    cidr_block = "10.0.0.0/18"    
 }
 
 resource "aws_internet_gateway" "dev-igw" {
@@ -23,4 +23,20 @@ resource "aws_internet_gateway" "dev-igw" {
 resource "aws_internet_gateway_attachment" "dev-igw-vpc" {
     internet_gateway_id = aws_internet_gateway.dev-igw.id
     vpc_id = aws_vpc.dev-vpc-01.id
+}
+
+resource "aws_subnet" "dev-pub-subnet-1" {
+    vpc_id = aws_vpc.dev-vpc-01.id
+    cidr_block = "10.0.1.0/24"
+    tags = {
+        Name = "dev-pub-subnet-1"
+    }
+}
+
+resource "aws_subnet" "dev-pub-subent-2" {
+    vpc_id = aws_vpc.dev-vpc-01.id
+    cidr_block = "10.0.2.0/24"
+    tags = {
+        Name = "dev-pub-subnet-2"
+    }
 }
